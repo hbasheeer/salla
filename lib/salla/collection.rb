310c3aab6661d63275856baf14e4698cba8eda7f
hbasheer <hasanbasher1989@gmail.com>
2022-01-09 01:07:00 +0200

    minor fixes

--- lib/salla/collection.rb
+++ lib/salla/collection.rb
@@ -1,21 +1,24 @@
 module Salla
   class Collection
-    attr_reader :data, :pagination
+    attr_reader :data, :pagination, :status, :error, :success
 
     def self.from_response(response, key:, type:)
       body = response.body
       new(
         data: body[key].map { |attrs| type.new(attrs) },
         success: body.dig["success"],
-        status: body.dig["status"]
+        status: body.dig["status"],
         error: body.dig["error"],
         pagination: body.dig("pagination")
       )
     end
 
-    def initialize(data:, pagination:)
+    def initialize(data:, pagination:, :error, :success, :status)
       @data = data
-      @pagination = pagination
+      @pagination = pagination.empty? ? nil : pagination
+      @status = status
+      @error = error.empty? ? nil : error
+      @status = status
     end
   end
 end

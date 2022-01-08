43f1cf44723097efe6184f9b11bf00f2d675fc07
hbasheer <hasanbasher1989@gmail.com>
2022-01-08 23:59:07 +0200

    update salla module

--- lib/salla.rb
+++ lib/salla.rb
@@ -1,6 +1,21 @@
+require "faraday"
+require "faraday_middleware"
 require "salla/version"
 
 module Salla
-  class Error < StandardError; end
-  # Your code goes here...
+  autoload :Client, "salla/client"
+  autoload :Collection, "salla/collection"
+  autoload :Error, "salla/error"
+  autoload :Resource, "salla/resource"
+  autoload :Object, "salla/object"
+
+  # High-level categories of salla API callsس
+  autoload :OrderResource, "salla/resources/order"
+  autoload :OrderStatusResource, "salla/resources/order_status"
+
+  # Classes used to return a nicer object wrapping the response data
+  autoload :Order, "salla/objects/order"
+  autoload :OrderStatus, "salla/objects/order_status"
+  autoload :OrderHistory, "salla/objects/order_history"
+
 end
